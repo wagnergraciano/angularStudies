15cd413e683a9061188946e6e17661903799ece9
Wagner Graciano Junior <wagner_graciano0101@hotmail.com>
2020-05-24 00:42:59 -0300

    Topic 1 finished

---
+++ projects/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { HEROES } from "../mock-heroes";
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  heroes = HEROES;
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
+
+  ngOnInit(): void {
+  }
+
+}
